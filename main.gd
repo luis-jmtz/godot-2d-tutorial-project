fad85f06c79e7c9c525e49df8795e32ad990b535
luis-jmtz <luis.jmtz2002@gmail.com>
2026-05-25 23:52:49 -0500

    added game_over function connected to the Player instance's hit signal

---
+++ main.gd
@@ -0,0 +1,12 @@
+extends Node
+
+@export var mob_scene: PackedScene
+# A simplified interface to a scene file. Provides access to operations and checks
+# that can be performed on the scene resource itself
+
+var score
+
+
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
